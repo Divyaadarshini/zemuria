ff0cc8c8614ca0e0db4f1c18d11bcfb9a466be5e
Divyaadarshini <54504425+Divyaadarshini@users.noreply.github.com>
2024-08-12 19:45:50 +0530

    Update test-page.js

--- middleware-test/pages/A/test-page.js
+++ middleware-test/pages/A/test-page.js
@@ -1,17 +1,10 @@
-//A-test-page
-import { useEffect, useState } from 'react';
-
-
-export default function TestPageA() {
-  const [divColor, setDivColor] = useState('blue');
-
-  useEffect(() => {
-    setDivColor('blue');
-  }, []);
+//B-test-page
+export default function TestPageB() {
+  const divColor = 'blue';
 
   return (
     <div>
-      <h1  style={{ color: divColor }}>This is version A of the test page.</h1>
+      <h1  style={{ color: divColor }}>This is version B of the test page.</h1>
     </div>
   );
 }
